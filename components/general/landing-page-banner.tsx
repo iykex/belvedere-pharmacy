ae640afc06f89f0fe9418a77f4dff6f99c7f6f0c
Dennis Sekyi Opoku <dennis.sekyi@resolutfinance.com>
2025-12-02 17:15:11 +0000

    style:restyling of landing page hero section

--- components/general/landing-page-banner.tsx
+++ components/general/landing-page-banner.tsx
@@ -3,7 +3,7 @@ import WidthConstraint from "@/components/shared/width-constraint";
 import { Badge } from "@/components/ui/badge";
 import { ABOUT_US_INFO_BANNER } from "@/lib/constants";
 import { ArrowRight, BadgeCheckIcon } from "lucide-react";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { useBannerAnimation } from "@/hooks/use-gsap";
 import { Button } from "../ui/button";
 import Link from "next/link";
@@ -17,12 +17,12 @@ export default function Banner() {
       className="bg-[url('/banner.png')] bg-cover bg-center h-screen overflow-x-hidden"
       ref={animationScope}
     >
-      <div className="w-full h-full bg-linear-to-r from-[#002f4b] from-2% flex items-center text-white overflow-hidden">
-        <WidthConstraint className="mt-20 sm:mt-0">
+      <div className="w-full h-full bg-linear-to-r from-[#002f4b] from-2% flex items-center text-white overflow-hidden lg:pt-20">
+        <WidthConstraint className="space-y-10 min-[500px]:space-y-15">
           <Badge
             id="NHS-badge"
             variant="secondary"
-            className="mb-4 sm:mb-8 p-2 text-sm md:text-base font-bold bg-[#00BFFF] text-[#002f4b] hover:bg-[#00BFFF]/90 border-0 px-4"
+            className="py-1.5 min-[300px]:px-4 max-[300px]:text-sm text-base font-bold bg-[#00BFFF] text-[#002f4b] hover:bg-[#00BFFF]/90 border-0"
           >
             <BadgeCheckIcon className="size-5! mr-2 stroke-3" />
             NHS Services Available
@@ -44,11 +44,11 @@ export default function Banner() {
               prescription services, and personalized care.
             </p>
           </div>
-          <div className="flex flex-col sm:flex-row gap-5 mt-6 sm:mt-12 sm:px-0">
+          <div className="flex flex-col min-[907px]:flex-row gap-4">
             <Button
               id="menu-outline-btn"
               variant="outline"
-              className="flex bg-transparent border-2 border-white text-white hover:bg-white hover:text-[#002f4b] w-fit sm:h-16 sm:px-10 rounded-full sm:text-xl font-bold transition-all duration-300 hover:shadow-lg hover:-translate-y-1"
+              className="flex bg-transparent border-2 border-white text-white hover:bg-white hover:text-[#002f4b] w-fit p-5 md:px-10 md:py-6 rounded-full min-[330px]:text-card-title font-bold transition-all duration-300 hover:shadow-lg hover:-translate-y-1"
             >
               <Link
                 href="https://app.belvederepharmacy.net/#/auth/signin"
@@ -60,11 +60,11 @@ export default function Banner() {
 
             <Button
               id="menu-solid-btn"
-              className="flex items-center justify-center bg-[#FF9900] text-white hover:bg-[#FF9900]/90 sm:h-16 sm:px-10 w-fit rounded-full sm:text-xl font-bold transition-all duration-300 shadow-xl hover:shadow-orange-500/30 hover:-translate-y-1 group border-2 border-[#FF9900]"
+              className="flex items-center justify-center bg-[#FF9900] text-white hover:bg-[#FF9900]/90 w-fit p-5 md:px-10 md:py-6  rounded-full min-[330px]:text-card-title font-bold transition-all duration-300 shadow-xl hover:shadow-orange-500/30 hover:-translate-y-1 group border-2 border-[#FF9900]"
             >
               <Link
                 href="https://shop.belvederepharmacy.net/appointments/viewallservices/all?pharmacy=378&type=redirection"
-                className="font-semibold flex items-center gap-x-2 hover:gap-x-4 transition-all duration-300 ease-in-out"
+                className="font-semibold flex items-center hover:gap-x-4 transition-all duration-300 ease-in-out"
               >
                 Book an Appointment{" "}
                 <ArrowRight className="hidden sm:flex ml-3 size-6 stroke-3 transition-transform group-hover:translate-x-1 group-hover:animate-bounce" />
@@ -72,7 +72,7 @@ export default function Banner() {
             </Button>
           </div>
 
-          <div id="info-badges" className=" mt-10 sm:mt-20 flex flex-wrap gap-5">
+          <div id="info-badges" className="flex flex-wrap gap-5">
             {ABOUT_US_INFO_BANNER.map((item) => {
               const Icon = item.icon;
               return (
