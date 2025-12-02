0363962ff63636ffe76f2d1cd509622daef30049
Dennis Sekyi Opoku <dennis.sekyi@resolutfinance.com>
2025-12-02 16:37:07 +0000

    style:restyling of landing page hero section

--- components/general/landing-page-banner.tsx
+++ components/general/landing-page-banner.tsx
@@ -17,18 +17,18 @@ export default function Banner() {
       className="bg-[url('/banner.png')] bg-cover bg-center h-screen overflow-x-hidden"
       ref={animationScope}
     >
-      <div className="w-full h-full bg-linear-to-r from-[#002f4b] from-2% flex items-center text-white">
-        <WidthConstraint>
+      <div className="w-full h-full bg-linear-to-r from-[#002f4b] from-2% flex items-center text-white overflow-hidden">
+        <WidthConstraint className="mt-20 sm:mt-0">
           <Badge
             id="NHS-badge"
             variant="secondary"
-            className="dark:bg-blue-600 mb-8 p-2 text-sm md:text-base font-bold bg-[#00BFFF] text-[#002f4b] hover:bg-[#00BFFF]/90 border-0 px-4"
+            className="mb-4 sm:mb-8 p-2 text-sm md:text-base font-bold bg-[#00BFFF] text-[#002f4b] hover:bg-[#00BFFF]/90 border-0 px-4"
           >
             <BadgeCheckIcon className="size-5! mr-2 stroke-3" />
             NHS Services Available
           </Badge>
 
-          <div className="max-w-4xl space-y-8">
+          <div className="max-w-4xl space-y-4 sm:space-y-8 w-full">
             <h1
               id="title"
               className="text-5xl md:text-6xl lg:text-8xl font-black text-white tracking-tight leading-[1.1] drop-shadow-xl font-sans"
@@ -44,11 +44,11 @@ export default function Banner() {
               prescription services, and personalized care.
             </p>
           </div>
-          <div className="flex flex-col sm:flex-row gap-5 mt-12 px-5 sm:px-0">
+          <div className="flex flex-col sm:flex-row gap-5 mt-6 sm:mt-12 sm:px-0">
             <Button
               id="menu-outline-btn"
               variant="outline"
-              className="flex items-center justify-center bg-transparent border-2 border-white text-white hover:bg-white hover:text-[#002f4b] h-16 px-10 rounded-full text-xl font-bold transition-all duration-300 hover:shadow-lg hover:-translate-y-1"
+              className="flex bg-transparent border-2 border-white text-white hover:bg-white hover:text-[#002f4b] w-fit sm:h-16 sm:px-10 rounded-full sm:text-xl font-bold transition-all duration-300 hover:shadow-lg hover:-translate-y-1"
             >
               <Link
                 href="https://app.belvederepharmacy.net/#/auth/signin"
@@ -60,19 +60,19 @@ export default function Banner() {
 
             <Button
               id="menu-solid-btn"
-              className="flex items-center justify-center bg-[#FF9900] text-white hover:bg-[#FF9900]/90 h-16 px-10 rounded-full text-xl font-bold transition-all duration-300 shadow-xl hover:shadow-orange-500/30 hover:-translate-y-1 group border-2 border-[#FF9900]"
+              className="flex items-center justify-center bg-[#FF9900] text-white hover:bg-[#FF9900]/90 sm:h-16 sm:px-10 w-fit rounded-full sm:text-xl font-bold transition-all duration-300 shadow-xl hover:shadow-orange-500/30 hover:-translate-y-1 group border-2 border-[#FF9900]"
             >
               <Link
                 href="https://shop.belvederepharmacy.net/appointments/viewallservices/all?pharmacy=378&type=redirection"
                 className="font-semibold flex items-center gap-x-2 hover:gap-x-4 transition-all duration-300 ease-in-out"
               >
                 Book an Appointment{" "}
-                <ArrowRight className="ml-3 size-6 stroke-3 transition-transform group-hover:translate-x-1 group-hover:animate-bounce" />
+                <ArrowRight className="hidden sm:flex ml-3 size-6 stroke-3 transition-transform group-hover:translate-x-1 group-hover:animate-bounce" />
               </Link>
             </Button>
           </div>
 
-          <div id="info-badges" className="mt-20 flex flex-wrap gap-5">
+          <div id="info-badges" className=" mt-10 sm:mt-20 flex flex-wrap gap-5">
             {ABOUT_US_INFO_BANNER.map((item) => {
               const Icon = item.icon;
               return (
@@ -80,11 +80,11 @@ export default function Banner() {
                   key={item.description}
                   className="flex items-center gap-4 backdrop-blur-xs  rounded-2xl p-0.5 hover:px-2 hover:bg-[#002f4b]/70 hover:border-[#00BFFF]/50 transition-all duration-500 group cursor-default"
                 >
-                  <div className="bg-[#00BFFF] p-3 rounded-xl shadow-lg group-hover:scale-110 transition-transform">
-                    <Icon className="size-6 text-[#002f4b] stroke-3" />
+                  <div className="bg-[#00BFFF] p-1.5 sm:p-3 rounded-xl shadow-lg group-hover:scale-110 transition-transform">
+                    <Icon className="size-4 sm:size-6 text-[#002f4b] stroke-3" />
                   </div>
                   <div>
-                    <p className="font-bold text-white leading-relaxed">
+                    <p className="font-semibold text-white leading-relaxed">
                       {item.title}
                     </p>
                     <p className="text-sm text-gray-300">{item.description}</p>
