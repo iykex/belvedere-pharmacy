a8af604e5766b72dfb76919a323c89b1db19409a
mr•vybes <aib.quame@gmail.com>
2026-08-05 04:18:33 +0000

    feat: add official pharmacy info and updated Pharmadoctor location links

---
+++ lib/constants/pharmacy-info.ts
@@ -0,0 +1,16 @@
+export const PHARMACY_INFO = {
+  name: "Belvedere Pharmacy",
+  slug: "belvedere",
+  postcode: "DA17 5QQ",
+  city: "Dartford",
+  region: "Kent",
+  pharmadoctorUrl:
+    "https://pharmadoctor.co.uk/patient/locations/london/dartford/BELVEDERE-PHARMACY--MECKAY-LIMITED--DA175QQ-2483",
+  shortPharmadoctorUrl: "https://www.phdr.co.uk/2483",
+  company: {
+    name: "MECKAY LIMITED",
+    registrationNumber: "06454698",
+    registeredIn: "UK",
+    address: "OAKHURST, ST PAULS WOOD HILL, BR5 2SR",
+  },
+} as const;
