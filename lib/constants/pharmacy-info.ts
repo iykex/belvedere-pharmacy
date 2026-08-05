3f43e6ce88627fed63d9b71cc518d5fc46522a82
mr•vybes <aib.quame@gmail.com>
2026-08-05 04:22:56 +0000

    feat: add verified phone number and full address to pharmacy-info

--- lib/constants/pharmacy-info.ts
+++ lib/constants/pharmacy-info.ts
@@ -1,9 +1,15 @@
 export const PHARMACY_INFO = {
   name: "Belvedere Pharmacy",
   slug: "belvedere",
-  postcode: "DA17 5QQ",
-  city: "Dartford",
-  region: "Kent",
+  phone: "020 8311 4087",
+  phoneHref: "tel:02083114087",
+  address: {
+    line1: "11 Picardy Street",
+    city: "Belvedere",
+    region: "Kent",
+    postcode: "DA17 5QQ",
+    country: "United Kingdom",
+  },
   pharmadoctorUrl:
     "https://pharmadoctor.co.uk/patient/locations/london/dartford/BELVEDERE-PHARMACY--MECKAY-LIMITED--DA175QQ-2483",
   shortPharmadoctorUrl: "https://www.phdr.co.uk/2483",
